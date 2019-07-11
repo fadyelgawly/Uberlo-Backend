72001650d8d8054ae0353b0f26c706f1c7394f55
Fady El Gawly <fgawly@gmail.com>
2019-07-11 20:05:57 +0200

    getrequested ride api made bug fix

--- router.js
+++ router.js
@@ -47,7 +47,7 @@ router.get('/getuserrides', (req, res, next) => {
 
 router.get('/getrequestedride', passport.authenticate('jwt', { session: false }), (req, res, next) => {
     const id = req.user.id;
-    db.query('SELECT * FROM ride WHERE rider = ? AND rideStatus != `D` AND rideStatus != `C`', [id], (err,rows) => {
+    db.query('SELECT * FROM ride WHERE rider = ? AND rideStatus != ? AND rideStatus != ?', [id, 'D', 'C'], (err,rows) => {
         if (err){
             res.status(500).json({
                 error: err.message
